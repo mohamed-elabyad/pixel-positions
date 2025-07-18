8588b3bc168fffdd44661595700b8172143dbbe6
mohamed-elabyad <m.elabyad.work@gmail.com>
2025-07-18 04:55:37 +0300

    Update RegisteredUserController.php

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -11,14 +11,6 @@ use Illuminate\Validation\Rules\Password;
 class RegisteredUserController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      */
     public function create()
@@ -56,35 +48,5 @@ class RegisteredUserController extends Controller
         return redirect('/');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
